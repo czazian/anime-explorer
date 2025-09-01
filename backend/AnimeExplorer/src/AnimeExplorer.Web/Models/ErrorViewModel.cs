d4585e388a516c288a0aec73e967382d621580bc
ささやかな風と青い海 <99159488+czazian@users.noreply.github.com>
2025-09-01 08:57:32 +0800

    - Initially Setup Backend - Added Constant for Frontend

---
+++ backend/AnimeExplorer/src/AnimeExplorer.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AnimeExplorer.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ backend/AnimeExplorer/tests/AnimeExplorer.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace AnimeExplorer.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
