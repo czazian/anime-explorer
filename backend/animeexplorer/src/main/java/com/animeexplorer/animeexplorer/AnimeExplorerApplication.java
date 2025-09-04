fdc4736d098bd87498fbbbda09a3cda446d53cd9
ささやかな風と青い海 <99159488+czazian@users.noreply.github.com>
2025-09-05 01:29:32 +0800

    Commit spring boot files

---
+++ backend/animeexplorer/src/main/java/com/animeexplorer/animeexplorer/AnimeExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.animeexplorer.animeexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeExplorerApplication.class, args);
+	}
+
+}
