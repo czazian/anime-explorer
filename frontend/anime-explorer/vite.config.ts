cff021a08517d51eee1fee090f18588355d1725d
ささやかな風と青い海 <99159488+czazian@users.noreply.github.com>
2025-09-06 16:52:27 +0800

    update vite.config.ts

--- frontend/anime-explorer/vite.config.ts
+++ frontend/anime-explorer/vite.config.ts
@@ -2,35 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    babel: {
+      plugins: ['@babel/plugin-transform-runtime']
+    }
+  })],
   build: {
-    chunkSizeWarningLimit: 1000,
     commonjsOptions: {
-      include: [/node_modules/],
+      include: [/node_modules/, /@babel\/runtime/],
       transformMixedEsModules: true
-    },
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
     }
   },
   optimizeDeps: {
-    include: [
-      '@babel/runtime/helpers/createSuper',
-      '@babel/runtime/helpers/inherits',
-      '@babel/runtime/helpers/createClass',
-      '@babel/runtime/helpers/assertThisInitialized',
-      '@babel/runtime/helpers/getPrototypeOf',
-      '@babel/runtime/helpers/possibleConstructorReturn',
-      'react-swipeable-views',
-      'react-swipeable-views-utils',
-      'react-material-ui-carousel'
-    ],
-  },
-  resolve: {
-    alias: {
-      '@babel/runtime': '@babel/runtime'
-    }
+    include: ['@babel/runtime']
   }
 })
