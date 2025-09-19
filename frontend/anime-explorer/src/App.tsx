25cdf19b7430e03ba75fbb180b46ecf82c0a4408
ささやかな風と青い海 <99159488+czazian@users.noreply.github.com>
2025-09-19 21:25:49 +0800

    Test vite config and babel/runtime issue

--- frontend/anime-explorer/src/App.tsx
+++ frontend/anime-explorer/src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css'
 import AppRoutes from "./routes/AppRoutes.tsx";
 import {Navbar} from "./share-component/Navbar.tsx";
-import {BrowserRouter} from "react-router-dom";
 import {DeviceProvider} from "./utils/MobileContext.tsx";
 import {ScrollToTop} from "./utils/ScrollToTop.ts";
 import {MessageService} from "./share-component/MessageService.tsx";
@@ -9,17 +8,15 @@ import {AuthProvider} from "./utils/AuthContext.tsx";
 
 function App() {
     return (
-        <BrowserRouter basename="/anime-explorer">
-            <DeviceProvider>
-                <MessageService>
-                    <AuthProvider>
-                        <Navbar/>
-                        <ScrollToTop/>
-                        <AppRoutes/>
-                    </AuthProvider>
-                </MessageService>
-            </DeviceProvider>
-        </BrowserRouter>
+        <DeviceProvider>
+            <MessageService>
+                <AuthProvider>
+                    <Navbar/>
+                    <ScrollToTop/>
+                    <AppRoutes/>
+                </AuthProvider>
+            </MessageService>
+        </DeviceProvider>
     );
 }
 
